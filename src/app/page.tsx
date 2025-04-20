aa49e9f76ae5fb526fe0444a8f2e851471865991
yoon664 <yoonj0264@gmail.com>
2025-04-20 10:24:08 +0900

    light, plane

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 'use client';
 
 import Three01 from "./three/three01";
+import Three02 from "./three/three02";
 
 export default function Home() {
   return (
     <>
-      <Three01 />
+      {/* <Three01 /> */}
+      <Three02 />
     </>
   );
 }
