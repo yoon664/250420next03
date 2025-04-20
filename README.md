ac8066af8a7a7277c68e73a85c5f5d435c981852
yoon664 <yoonj0264@gmail.com>
2025-04-20 09:47:01 +0900

    mesh,sphere,cone,box,cylinder

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # Three.js Fiber Drei
+next 14버전 추천?
 
 Three.js : 브라우저에서 3D 그래픽을 쉽게 렌더링할 수 있도록 도와주는 javascript 라이브러리
+
+React Fiber : Three.js를 React 생태계 안에서 사용할 수 있도록 도와주는 라이브러리
+
+Drei : Fiber를 기반으로 한 유틸리티 모음 라이브러리 (자주 사용되는 3D 기능들을 미리 컴포넌트로 만들어 놓아 개발 생산성을 높여줌)
+
+설치 : npm i three @react-three/fiber @react-three/drei
+@react-three/fiber@8 @react-three/drei@9 _ 버전 문제로 전 버전으로
+
+---
---
+++ src/app/three/three01.tsx
@@ -0,0 +1,35 @@
+import { Sphere } from '@react-three/drei'
+import { Canvas } from '@react-three/fiber'
+import React from 'react'
+
+const Three01 = () => {
+  return (
+    <div className='h-screen bg-yellow-400'>
+        {/* Fiber의 핵심 컴포넌트, 3D씬을 렌더링하는 공간을 제공 */}
+        <Canvas>
+            {/* 주변광 추가 */}
+            <ambientLight intensity={10} />
+
+            {/* mesh : 3D객체의 기본 단위 */}
+            <mesh position={[-2,0,0]}>  {/* x축 y축 z축 */}
+                <sphereGeometry args={[1,32,32]} />
+                <meshStandardMaterial color='red' />
+            </mesh>
+
+            {/* 
+                [ Drei의 Primitive 컴포넌트 ]
+                - mesh 래퍼가 필요없음
+                - geometry와 material을 자동으로 생성
+                (custom material을 자식으로 추가 가능)
+                - position, scale, rotation 속성을 직접 받을 수 있음
+            */}
+            <Sphere position={[-4,0,0]} args={[1,32,32]}>
+                <meshStandardMaterial color='red' />
+            </Sphere>
+
+        </Canvas>
+    </div>
+  )
+}
+
+export default Three01
